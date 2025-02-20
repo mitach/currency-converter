7b745d3a580418ac69062b34cca6228dd4ccf45c
Dimitar Malinov <malinovd02@gmail.com>
2025-02-20 09:46:40 +0200

    fix: fix cors in backend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -12,11 +12,22 @@ const app = express();
 
 const PORT = process.env.PORT || 5500;
 
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://famous-chaja-81bb8b.netlify.app'
+];
+
 app.use(cors({
-  origin: [
-    'http://localhost:5173',
-    'https://famous-chaja-81bb8b.netlify.app/'
-  ]
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, true);
+    
+    if (allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true
 }));
 app.use(express.json());
 
